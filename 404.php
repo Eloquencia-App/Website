d73889167256881864951d65b061536563196e5d
Ethan <41381482+ethandudu@users.noreply.github.com>
2024-08-30 11:59:16 +0200

    added 404 page

---
+++ 404.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Eloquéncia est une association loi 1901 visant à promouvoir l'éloquence et l'art oratoire">
+    <meta name="keywords" content="éloquence, oratoire, association, loi 1901, parler en public, discours, formation, cours en ligne">
+    <meta name="author" content="Eloquéncia">
+    <meta name="robots" content="index, follow">
+    <meta name="revisit-after" content="7 days">
+    <meta name="language" content="fr">
+    <meta property="og:site_name" content="Eloquéncia">
+    <meta property="og:site" content="https://eloquencia.org">
+    <meta property="og:title" content="404">
+    <meta property="og:description" content="Eloquéncia est une association loi 1901 visant à promouvoir l'éloquence et l'art oratoire">
+    <title>404 - Eloquéncia</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/bootstrap.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="./">
+            <img src="assets/eloquencia_round.png" alt="Logo" width="64" height="64" class="d-inline-block">
+            Eloquéncia
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="navbar-nav">
+                <a class="nav-link" href="./">Accueil</a>
+                <a class="nav-link" href="./#about">À propos</a>
+                <a class="nav-link" href="./#contact">Contact</a>
+                <a class="nav-link" href="discount">Réduction</a>
+                <a class="nav-link" href="#">Connexion</a>
+            </div>
+        </div>
+    </div>
+</nav>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="display-1 text-center">Eloquéncia</h1>
+                <p class="lead text-center">La plateforme de cours en ligne pour apprendre à parler en public</p>
+                <div class="d-flex justify-content-center">
+                    <a href="#about" class="btn btn-primary btn-lg">Découvrir</a>
+                    <a href="https://www.helloasso.com/associations/eloquencia/adhesions/adhesion" class="btn btn-secondary btn-lg">Adhérer</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<hr class="my-4">
+<div id="404" class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2 class="display-4 text-center">404</h2>
+            <p class="lead text-center">La page que vous cherchez n'existe pas. <a href="./">Retour à l'accueil</a></p>
+        </div>
+    </div>
+</div>
+<hr class="my-4">
+<!-- Footer -->
+<footer class="bg-body-tertiary text-center text-lg-start footer fixed-bottom">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2024 Eloquéncia | Fait avec ❤️ et hébergé en France
+    </div>
+</footer>
+</body>
+</html>
