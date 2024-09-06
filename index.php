56ec0fc30f18ed8d561e908196ff14276c1ba786
Ethan <41381482+ethandudu@users.noreply.github.com>
2024-09-06 22:23:09 +0200

    edited album

--- index.php
+++ index.php
@@ -91,9 +91,9 @@ if(isset($_POST['captcha'])) {
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                 <div class="col">
                     <div class="card shadow-sm">
-                        <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                        <img src="assets/vignette_eloquence.png" alt="Vignette éloquence" width="100%" height="225">
                         <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text">L'éloquence est un art oratoire qui permet de valoriser la prise de parole en public par des techniques et autres.</p>
                         </div>
                     </div>
                 </div>
